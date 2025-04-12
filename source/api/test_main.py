e3f3d527203cb51d815a251deb55e797272a3d5c
phamdinhkhanh <phamdinhkhanh.tkt53.neu@gmail.com>
2025-04-12 10:05:33 +0700

    Update test cases

--- source/api/test_main.py
+++ source/api/test_main.py
@@ -8,6 +8,7 @@ import os
 import sys
 import pathlib
 from source.api.main import app
+import time
 
 # Instantiate the testing client with our app.
 client = TestClient(app)
@@ -20,7 +21,6 @@ def test_root():
 # a unit test that tests the status code and response 
 # for an instance with a low income
 def test_get_inference_fully_paid():
-
     person = {
         "loan_amnt": 5000.0,
         "term": '36 months',
@@ -49,7 +49,6 @@ def test_get_inference_fully_paid():
 # a unit test that tests the status code and response 
 # for an instance with a high income
 def test_get_inference_charged_off():
-
     person = {
         "loan_amnt": 21000.0,
         "term": '36 months',
@@ -74,3 +73,85 @@ def test_get_inference_charged_off():
     print(r.json())
     assert r.status_code == 200
     assert r.json() == "Charged Off"
+
+
+# a unit test that tests the status code of edge case and error handling tests
+def test_get_inference_edge_case():
+    # load_amnt is incorrect format, which is string.
+    person = {
+        "loan_amnt": "21000.0",
+        "term": '36 months',
+        "int_rate": 12.42,
+        "installment": 701.73,
+        "emp_length": "10+ years",
+        "home_ownership": "RENT",
+        "annual_inc": 105000,
+        "verification_status": "Verified",
+        "purpose": "debt_consolidation",
+        "dti": 13.22,
+        "delinq_2yrs": 0.0,
+        "inq_last_6mths": 0.0,
+        "open_acc": 7.0,
+        "pub_rec": 0.0,
+        "revol_bal": 32135.0,
+        "revol_util": 90.3,
+        "total_acc": 38.0
+    }
+
+    r = client.post("/predict", json=person)
+    assert r.status_code == 200
+
+# a unit test that tests the status code of edge case and error handling tests incorrect key
+def test_get_inference_error_handling_incorrect_key():
+    # term is mispelled into tern.
+    person = {
+        "loan_amnt": 21000.0,
+        "tern": '36 months',
+        "int_rate": 12.42,
+        "installment": 701.73,
+        "emp_length": "10+ years",
+        "home_ownership": "RENT",
+        "annual_inc": 105000,
+        "verification_status": "Verified",
+        "purpose": "debt_consolidation",
+        "dti": 13.22,
+        "delinq_2yrs": 0.0,
+        "inq_last_6mths": 0.0,
+        "open_acc": 7.0,
+        "pub_rec": 0.0,
+        "revol_bal": 32135.0,
+        "revol_util": 90.3,
+        "total_acc": 38.0
+    }
+
+    r = client.post("/predict", json=person)
+    assert r.status_code == 422
+
+# a unit test that tests the status code and inference time of single inference.
+def test_inference_time():
+    # inference time must be less than 3 seconds.
+    person = {
+        "loan_amnt": 21000.0,
+        "term": '36 months',
+        "int_rate": 12.42,
+        "installment": 701.73,
+        "emp_length": "10+ years",
+        "home_ownership": "RENT",
+        "annual_inc": 105000,
+        "verification_status": "Verified",
+        "purpose": "debt_consolidation",
+        "dti": 13.22,
+        "delinq_2yrs": 0.0,
+        "inq_last_6mths": 0.0,
+        "open_acc": 7.0,
+        "pub_rec": 0.0,
+        "revol_bal": 32135.0,
+        "revol_util": 90.3,
+        "total_acc": 38.0
+    }
+    start_time = time.time()
+    r = client.post("/predict", json=person)
+    # print(r.json())
+    seconds = time.time() - start_time
+    assert seconds < 3
+    assert r.status_code == 200
